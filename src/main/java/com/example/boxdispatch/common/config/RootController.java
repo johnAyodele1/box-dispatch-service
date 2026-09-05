d2e23e4e0e1878cdf9d1165051ffcd57e1cbca16
John Ayodele <johhelporg@gmail.com>
2026-09-05 08:12:35 +0100

    Redirect API root to Swagger UI

--- src/main/java/com/example/boxdispatch/common/config/RootController.java
+++ src/main/java/com/example/boxdispatch/common/config/RootController.java
@@ -1,20 +1,13 @@
 package com.example.boxdispatch.common.config;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
-@RestController
+@Controller
 public class RootController {
 
     @GetMapping("/")
-    public Map<String, String> root() {
-        return Map.of(
-            "service", "Box Dispatch Service",
-            "status", "UP",
-            "swagger", "/swagger-ui.html",
-            "openapi", "/v3/api-docs"
-        );
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui.html";
     }
 }
